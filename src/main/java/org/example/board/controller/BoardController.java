a4024dfe1313a174e8e37428f591a072dfbf34fc
Jaeyoung <jjy0843@gmail.com>
2024-05-24 01:59:06 +0900

    view, update, delete 에 비밀번호 다를때 오류처리 추가

--- src/main/java/org/example/board/controller/BoardController.java
+++ src/main/java/org/example/board/controller/BoardController.java
@@ -1,9 +1,9 @@
 package org.example.board.controller;
 
 
+import lombok.RequiredArgsConstructor;
 import org.example.board.domain.Board;
 import org.example.board.service.BoardService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
@@ -12,10 +12,11 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/board")
+@RequiredArgsConstructor
 public class BoardController {
 
-    @Autowired
-    private BoardService boardService;
+
+    private final BoardService boardService;
 
     @GetMapping("/list")
     public String list(Model model, @RequestParam(defaultValue = "0") int page) {
@@ -27,9 +28,12 @@ public class BoardController {
     }
 
     @GetMapping("/view")
-    public String view(Model model, @RequestParam Long id) {
+    public String view(Model model,@RequestParam(required = false) String error,@RequestParam Long id) {
         Board board = boardService.findById(id);
         model.addAttribute("board", board);
+        if (error != null && error.equals("wrongPassword")) {
+            model.addAttribute("errorMessage", "비밀번호가 다릅니다.");
+        }
         return "board/view";
     }
 
@@ -53,23 +57,16 @@ public class BoardController {
 
     @PostMapping("/update")
     public String update(@ModelAttribute Board board, @RequestParam String password) {
-        Board existingBoard = boardService.findById(board.getId());
-
-        if (!existingBoard.getPassword().equals(password)) {
-            return "redirect:/board/view?id=" + board.getId();
+        Board updatedBoard = boardService.update(board, password);
+        if (updatedBoard == null) {
+            return "redirect:/board/view?id=" + board.getId() + "&error=wrongPassword";
         }
-        existingBoard.setName(board.getName());
-        existingBoard.setTitle(board.getTitle());
-        existingBoard.setContent(board.getContent());
-
-        boardService.update(existingBoard);
-
         return "redirect:/board/view?id=" + board.getId();
     }
 
-
     @GetMapping("/deleteform")
     public String deleteForm(Model model, @RequestParam Long id) {
+
         model.addAttribute("id", id);
         return "board/deleteform";
     }
@@ -79,7 +76,7 @@ public class BoardController {
         Board board = boardService.findById(id);
         if (board.getPassword().equals(password)) {
             boardService.deleteById(id);
-        }
+        }else return "redirect:/board/view?id=" + board.getId() + "&error=wrongPassword";
         return "redirect:/board/list";
     }
 }
