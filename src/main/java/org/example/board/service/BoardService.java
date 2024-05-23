efd7460ea69792224dfe0a2fde3e8f071ab4c933
Jaeyoung <jjy0843@gmail.com>
2024-05-24 01:03:45 +0900

    BoardService findAll 시간 역순 정렬 수정

--- src/main/java/org/example/board/service/BoardService.java
+++ src/main/java/org/example/board/service/BoardService.java
@@ -5,7 +5,9 @@ import org.example.board.domain.Board;
 import org.example.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -18,7 +20,10 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     public Page<Board> findAll(Pageable pageable) {
-        return boardRepository.findAll(pageable);
+        Pageable sortedByDescId = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(),
+                Sort.by(Sort.Direction.DESC, "createdAt"));
+
+        return boardRepository.findAll(sortedByDescId);
     }
 
     public Board findById(Long id) {
